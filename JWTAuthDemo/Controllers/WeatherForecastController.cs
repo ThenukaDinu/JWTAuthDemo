2f6cc27f673d738fc1add023181285bc1554ef66
Thenukad Dinu <thenukadinu@gmail.com>
2022-01-09 20:39:12 +0530

    JWT DEMO dotnet core.

---
+++ JWTAuthDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,76 @@
+﻿using JWTAuthDemo.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Authenticate")]
+        public IActionResult Authenticate()
+        {
+            // Generate the JWT token and return
+
+            // Claims
+            var claims = new[]
+            {
+                new Claim("FullName", "Thenuka Dinu Jayarathna"),
+                new Claim(JwtRegisteredClaimNames.Sub, "user_Id")
+            };
+
+            var keyBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(keyBytes);
+
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                Constants.Audiance,
+                Constants.Issuer,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: signingCredentials);
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new { accessToken = tokenString });
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ JWTAuthDemo/Helpers/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace JWTAuthDemo.Helpers
+{
+    public static class Constants
+    {
+        public const string Audiance = "https://localhost:44335";
+        public const string Issuer = "https://localhost:44335";
+        public const string Secret = "EWFA232wcxse%$^^&*#fesr354345#@$#WQEFC#@R@#Cwfdsg436t34523DC2q1313123dsacD@#p{}}'?()";
+    }
+}
